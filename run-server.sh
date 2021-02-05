d75fcaab0e87d71c5253e55e64258451132d8937
John Basila <jbasila@gmail.com>
2021-02-05 15:36:46 +0200

    Adding port

--- run-server.sh
+++ run-server.sh
@@ -6,7 +6,7 @@ usage() {
 	(( ${#message} )) && echo "${message}"
 
 	echo "
-usage: $(basename $0) --server-name SERVERNAME [--image IMAGE] [--uid UID] [--java-ops JAVAOPS]
+usage: $(basename $0) --server-name SERVERNAME [--image IMAGE] [--uid UID] [--port PORT] [--java-ops JAVAOPS]
 "
 }
 
@@ -14,7 +14,8 @@ run-server() {
 	docker_image="${1}"
 	server_name="${2}"
 	user_uid="${3}"
-	java_opt="${4}"
+	port="${4}"
+	java_opt="${5}"
 
 	server_path="${HOME}/minecraft/${server_name}"
 	[[ ! -d "${server_path}" ]] && echo "path '${server_path}' does not point to a directory" && exit 1
@@ -25,6 +26,7 @@ run-server() {
 		--volume ${server_path}:/mc-server \
 		--volume /etc/passwd:/etc/passwd:ro \
 		--volume /etc/group:/etc/group:ro \
+		-p ${port}:25565 \
 		"${docker_image}" \
 		--workdir /mc-server \
 		--java-ops "${java_ops}"
@@ -33,6 +35,7 @@ run-server() {
 main() {
 	docker_image="mc:latest"
 	user_uid=$(id -u)
+	port=25565
 	java_ops="-Xmx2G"
 
 	while (( ${#})); do
@@ -52,6 +55,11 @@ main() {
 				shift
 				server_name="${1}"
 				;;
+			--port)
+				(( ${#} < 2 )) && usage "option '${1}' takes an argument" && exit 1
+				shift
+				port="${1}"
+				;;
 			--java-ops)
 				(( ${#} < 2 )) && usage "option '${1}' takes an argument" && exit 1
 				shift
@@ -68,7 +76,7 @@ main() {
 
 	(( ${#server_name} == 0 )) && usage "Must supply server_name" && exit 1
 	
-	run-server "${docker_image}" "${server_name}" "${user_uid}" "${java_ops}"
+	run-server "${docker_image}" "${server_name}" "${user_uid}" "${port}" "${java_ops}"
 }
 
 main "${@}"
